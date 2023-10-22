c7f28e46c0fea1a1f8193951b39cecc0c7411b86
Chris <cx72@cornell.edu>
2023-10-22 16:40:28 -0400

    Filling in Init
    - Create .git directory
    - Raise exception if already exist

--- commands/init.ml
+++ commands/init.ml
@@ -1,5 +1,13 @@
 open Utils
 
 module Init : Command.EmptyCommand = struct
-  let run () : string = "Initialized empty repository."
+  let run () : string =
+    if Sys.file_exists ".got" then
+      raise
+        (Failure
+           "A Got version-control system already exists in the current \
+            directory.")
+    else
+      let () = Sys.mkdir ".got" 0o755 in
+      "Initialized empty repository."
 end
