8fd130f6212de8b0dee0d9335c6abd48c0675cbe
Chris <cx72@cornell.edu>
2023-10-23 01:15:04 -0400

    Finishing add

--- commands/add.ml
+++ commands/add.ml
@@ -1,46 +1,78 @@
 open Utils
 
 type file_metadata = {
+  name : string;
   hash : int;
   contents : string;
 }
 
 module Add : Command.ArgCommand = struct
   let string_of_file file =
-    if Sys.file_exists file then (
-      let ic = open_in file in
-      let buf = Buffer.create (in_channel_length ic) in
-      try
-        while true do
-          let line = input_line ic in
-          Buffer.add_string buf line
-        done;
-        assert false (* This line is not expected to be reached *)
-      with
-      | End_of_file ->
-          close_in ic;
-          Buffer.contents buf
-      | e ->
-          close_in ic;
-          (* Make sure to close the input channel in case of any other
-             exceptions *)
-          raise e)
-    else raise (Failure "File does not exist")
+    let ic = open_in (Constants.repo_root () ^ file) in
+    let n = in_channel_length ic in
+    let s = Bytes.create n in
+    really_input ic s 0 n;
+    close_in ic;
+    Bytes.to_string s
 
-  let add stage file =
-    if Sys.file_exists file then
-      let out_channel = open_out stage in
-      output_string out_channel
-        (file ^ ", "
-        ^ string_of_int (Utils.Hash.hash file)
-        ^ ", " ^ string_of_file file)
-    else raise (Failure "File does not exist")
+  let rec get_added_files files =
+    match files with
+    | [] -> []
+    | file :: tl ->
+        if Sys.file_exists (Constants.repo_root () ^ file) then
+          file :: get_added_files tl
+        else raise (Failure (file ^ " does not exist"))
+
+  let get_staged_metadata () : file_metadata list =
+    let stage = Constants.repo_root () ^ ".got/stage.msh" in
+    let in_channel = open_in stage in
+    try
+      let metadata = Marshal.from_channel in_channel in
+      close_in in_channel;
+      metadata
+    with End_of_file ->
+      close_in in_channel;
+      []
+
+  let rec update_metadata (file : string) metadata =
+    match metadata with
+    | data :: t ->
+        if data.name = file then
+          {
+            name = file;
+            hash = Utils.Hash.hash file;
+            contents = string_of_file file;
+          }
+          :: update_metadata file t
+        else data :: update_metadata file t
+    | [] ->
+        [
+          {
+            name = file;
+            hash = Utils.Hash.hash file;
+            contents = string_of_file file;
+          };
+        ]
+
+  let add_file_metadata files =
+    let rec add_file_metadata_helper files metadata =
+      match files with
+      | [] -> metadata
+      | h :: t -> add_file_metadata_helper t (update_metadata h metadata)
+    in
+    add_file_metadata_helper files (get_staged_metadata ())
+
+  let marshal_to_file files =
+    let out_channel = open_out (Constants.repo_root () ^ ".got/stage.msh") in
+    Marshal.to_channel out_channel
+      (add_file_metadata (get_added_files files))
+      [];
+    close_out out_channel
 
   let run files =
-    let stage = ".got/stage.txt" in
     match files with
-    | [] -> "No file added!"
-    | file :: t ->
-        add stage file;
-        "Added " ^ file
+    | [] -> "No file added."
+    | _ ->
+        let _ = marshal_to_file files in
+        "Added " ^ String.concat " " files
 end
---
+++ utils/constants.ml
@@ -0,0 +1,1 @@
+let repo_root () = "repo/"
