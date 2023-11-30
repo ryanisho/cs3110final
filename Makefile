f489c398f6e490ae82dff9dd3fb94f155e81f072
Ryan Ho <rh564@cornell.edu>
2023-11-30 03:54:22 -0500

    Refactored Makefile
    and added working test suite

--- Makefile
+++ Makefile
@@ -1,19 +1,17 @@
-.PHONY: got clean build
-
-default: build
-
-clean: 
-	rm -rf repo/.got/
+.PHONY: test clean got
 
 build:
 	dune build
 
+test:
+	OCAMLRUNPARAM=b dune exec test/main.exe
+
+clean: 
+	rm -rf repo/.got/
+
 # Commands
 got:
 	dune exec bin/main.exe $(filter-out $@,$(MAKECMDGOALS))
-
-test:
-	dune exec test/main.exe
 
 # Generic rule to suppress "make: *** No rule to make target `$got-cmd'.  Stop." message
 # Note that this also suppresses real unknown targets though.
--- test/main.ml
+++ test/main.ml
@@ -2,12 +2,17 @@ open OUnit2
 open Commands
 
 (* helper test functions *)
+let test_run_no_files _ =
+  let result = Commands.Add.run [] in
+  assert_equal "No file added." result
 
 (* main test suite *)
 let tests = [
-  (* tests go here *)
+  "test suite for 'run' function" >::: [
+    "test with no files" >:: test_run_no_files;
+  ]
 ]
 
 (* test suite driver *)
-let suite = "suite" >::: tests
-let () = run_test_tt_main suite
+let suite = "got test suite" >::: tests
+let _ = run_test_tt_main suite
