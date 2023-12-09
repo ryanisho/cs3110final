55b841ae3cfb9057653932b37fe92de65a56f3d5
chrisfxu <98923489+chrisfxu@users.noreply.github.com>
2023-12-09 17:03:17 -0500

    Refactor (#9)
    * text coloring and indenting for status
    
    * Checking got repo initialization before running commands
    and mli files
    
    * Blob helpers and make doc command for Makefile
    
    * Add writing to /blobs on add

--- utils/commit.ml
+++ utils/commit.ml
@@ -24,9 +24,9 @@ let write_commit (stage : Stage.t) (message : string) : string =
       parent = retrieve_latest_commit_filename ();
       merge_parent = None;
       changes =
-        stage |> List.map (fun (file_metadata : Stage.file_metadata) ->
-            (file_metadata.name, file_metadata.hash));
-
+        stage
+        |> List.map (fun (file_metadata : Stage.file_metadata) ->
+               (file_metadata.name, file_metadata.hash));
     }
   in
   (Filesystem.marshal_data_to_file : t -> string -> unit)
@@ -38,6 +38,12 @@ let write_commit (stage : Stage.t) (message : string) : string =
 let fetch_commit (timestamp : Filesystem.filename) : t =
   Filesystem.marshal_file_to_data (Filesystem.Repo.commit_dir () ^ timestamp)
 
+let fetch_latest_commit () =
+  retrieve_latest_commit_filename () |> Option.get |> fetch_commit
+
+let fetch_latest_commit_changes () =
+  fetch_latest_commit () |> fun c -> c.changes
+
 let get_full_commit_history () : t list =
   retrieve_all_commit_filenames ()
   |> List.map fetch_commit
@@ -46,10 +52,9 @@ let get_full_commit_history () : t list =
 
 let clear_commit_history () =
   let commit_files = retrieve_all_commit_filenames () in
-  List.iter (fun filename -> 
-      try
-        Sys.remove (Filesystem.Repo.commit_dir () ^ filename)
-      with
-      | Sys_error msg -> print_endline ("Failed to delete file " ^ filename ^ ": " ^ msg)
-    ) commit_files
-
+  List.iter
+    (fun filename ->
+      try Sys.remove (Filesystem.Repo.commit_dir () ^ filename)
+      with Sys_error msg ->
+        print_endline ("Failed to delete file " ^ filename ^ ": " ^ msg))
+    commit_files
