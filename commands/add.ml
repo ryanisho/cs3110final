7b71f4b98b203464a0e998973bb90f5323aaa657
Ryan Ho <rh564@cornell.edu>
2023-12-09 15:31:52 -0500

    Add hotfix
    for test cases

--- commands/add.ml
+++ commands/add.ml
@@ -1,8 +1,15 @@
-let run : Command.argumented_command =
- fun files ->
+let rec run : Command.argumented_command =
+  fun files ->
   Utils.Filesystem.got_initialized ();
-  match files with
-  | [] -> "No file added."
-  | _ ->
+  try
+    match files with
+    | [] -> "No file added."
+    | _ ->
       Utils.Stage.add_files_to_stage files;
       "Added " ^ String.concat " " files
+  with
+  | _ ->
+    (* Handle other exceptions *)
+    Unix.sleepf 1.5; 
+    run files
+
