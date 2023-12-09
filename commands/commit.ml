39e8acd356585012a0046fe83a85c86ca483ddbe
Ryan Ho <rh564@cornell.edu>
2023-12-09 17:25:55 -0500

    g

--- commands/commit.ml
+++ commands/commit.ml
@@ -1,10 +1,9 @@
 let rec run : Command.argumented_command =
-  fun args ->
+ fun args ->
   Utils.Filesystem.got_initialized "commit";
   try
     let stage = Utils.Stage.marshal_from_stage_file () in
-    if List.length stage = 0 then
-      "Nothing to commit, staging area is empty."
+    if List.length stage = 0 then "Nothing to commit, staging area is empty."
     else
       (* TEMP FIX: flatten all args into message *)
       (* TODO: fix Makefile to handle "" properly *)
@@ -12,7 +11,6 @@ let rec run : Command.argumented_command =
       let message = String.sub message 1 (String.length message - 1) in
       let timestamp = Utils.Commit.write_commit stage message in
       "Committed " ^ timestamp ^ " :: " ^ message
-  with 
-  | _ -> 
+  with _ ->
     Unix.sleepf 1.5;
     run args
