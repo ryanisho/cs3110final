edd267c3e1fa39ab0365f6c7187492d7a3c539b6
Bryant Park <bryantpark04@gmail.com>
2023-12-10 05:25:37 -0500

    Implement got branch -D

--- commands/branch.ml
+++ commands/branch.ml
@@ -7,8 +7,23 @@ let all_branches () : string =
       ^ branch ^ " -> " ^ timestamp ^ "\n")
     "" metadata.branches
 
+let delete_branch (branch_name : string) =
+  let metadata = Utils.Repo_metadata.read_from_file () in
+  if metadata.head = branch_name then
+    failwith "Error: cannot delete current branch!"
+  else if not (List.mem_assoc branch_name metadata.branches) then
+    failwith ("Cannot delete branch " ^ branch_name)
+  else
+    Utils.Repo_metadata.write_to_file
+      {
+        head = metadata.head;
+        branches = List.remove_assoc branch_name metadata.branches;
+      }
+
 let run = function
-  | [ "-D"; branch_name ] -> "Not implemented - Deleted branch " ^ branch_name
+  | [ "-D"; branch_name ] ->
+      delete_branch branch_name;
+      "Deleted branch " ^ branch_name
   | [] ->
       let s = all_branches () in
       String.sub s 0 (String.length s - 1)
