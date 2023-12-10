6e5795a6e0701321b0bae89839a3ee865ac87eb8
chrisfxu <98923489+chrisfxu@users.noreply.github.com>
2023-12-09 22:39:09 -0500

    Exception handling (#11)
    * exception handling
    
    * rm setup and exception handling
    
    * FIX FIX FIX FIX FIX

--- commands/rm.mli
+++ commands/rm.mli
@@ -1,15 +1,6 @@
 (* rm.mli *)
 
-<<<<<<< HEAD
 val check_tracked : string list -> unit
-=======
-(**  [rm files] removes the given list of files from the stage. Returns a
-     string summarizing the actions taken. *)
-
-val is_tracked : string list -> string list
->>>>>>> origin/main
-(** [is_tracked files] checks if the given list of files are tracked, and
-    returns the list of tracked files. Raises Failure if a file is not tracked. *)
 
 val remove : string list -> string
 (** [remove files] removes the given list of files from the stage. Returns a
