6a2dd4b7d410014af777987c47407c15ae24e1ed
Bryant Park <bryantpark04@gmail.com>
2023-12-09 23:52:15 -0500

    Use repo metadata when committing
    - Add write_initial_commit function

--- utils/repo_metadata.ml
+++ utils/repo_metadata.ml
@@ -1,13 +1,13 @@
 type t = {
-  head : Filesystem.filename option;
+  head : Filesystem.filename;
   branches : (string * Filesystem.filename) list;
 }
 
 let write_to_file (m : t) =
   Filesystem.marshal_data_to_file m (Filesystem.Repo.metadata_file ())
 
+let read_from_file () : t =
+  Filesystem.marshal_file_to_data (Filesystem.Repo.metadata_file ())
+
 let empty (initial_commit_timestamp : Filesystem.filename) =
-  {
-    head = Some initial_commit_timestamp;
-    branches = [ ("master", initial_commit_timestamp) ];
-  }
+  { head = "master"; branches = [ ("master", initial_commit_timestamp) ] }
